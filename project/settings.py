505fd4ecae36d83e48006eb4aab3fc9b6baab7d9
Эдуард <alexed34@yandex.ru>
2020-02-23 15:09:30 +0400

    Применил DJ-Database-URL

--- project/settings.py
+++ project/settings.py
@@ -2,24 +2,12 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 import environs
-#import dj_database_url
+import dj_database_url
 
 env = environs.Env()
 
 
-#DATABASES = {'default': dj_database_url.config(default=os.environ['DATABASE_URL'])}
-
-DATABASES = {
-    'default': {
-        'ENGINE': os.getenv('ENGINE'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT'),
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD')
-    }
-}
-
+DATABASES = {'default': dj_database_url.config(default=os.environ['DATABASE_URL'])}
 
 
 INSTALLED_APPS = ['datacenter']
